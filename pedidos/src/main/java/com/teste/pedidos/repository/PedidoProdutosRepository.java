5ef8a8dffe61bd256e323a8fae26cab82d6d5496
igorbavand <igor.bvn@gmail.com>
2022-05-26 01:05:35 -0300

    feat: mapeando pedido produtos

---
+++ pedidos/src/main/java/com/teste/pedidos/repository/PedidoProdutosRepository.java
@@ -0,0 +1,12 @@
+package com.teste.pedidos.repository;
+
+import com.teste.pedidos.model.Pedido;
+import com.teste.pedidos.model.PedidoProdutos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PedidoProdutosRepository  extends JpaRepository<PedidoProdutos, UUID> {
+}
