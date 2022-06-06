696dc9eeee843706ba18634fcb9c11cef9f5cf38
igorbavand <igor.bvn@gmail.com>
2022-06-05 21:41:59 -0300

    feat: adicionando EmailPedidoDto

---
+++ libs/src/main/java/dto/EmailPedidoDto.java
@@ -0,0 +1,15 @@
+package dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+public class EmailPedidoDto implements Serializable {
+
+    private String emailDestinatario;
+    private String assunto;
+    private UUID codigoPedido;
+    private String nomeCliente;
+}
